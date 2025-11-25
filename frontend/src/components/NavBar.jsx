2090032e36aabd0897307d6e976e2adfcbf7c5ad
SadisaRashmika <sadisa.rashmika@gmail.com>
2025-11-25 17:39:18 +0530

    home page view

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import logo from '../assets/assets/logo.png'
-import { MenuIcon, SearchIcon, XIcon } from 'lucide-react'
+import { MenuIcon, SearchIcon, TicketPlus, XIcon } from 'lucide-react'
 import { useState } from 'react'
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
@@ -12,6 +12,8 @@ const NavBar = () => {
     const[isOpen, setIsOpen] = useState(false)
     const {user}=useUser()
     const{openSignIn}=useClerk()
+
+    const navigate = useNavigate()
 
   return (
     <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
@@ -28,11 +30,11 @@ const NavBar = () => {
 
             <XIcon className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer' onClick={()=>setIsOpen(!isOpen)}/>
 
-            <Link onClick={()=>{scrollTo(0,0),setIsOpen(false)}} to ='/'>Home</Link>
-            <Link onClick={()=>{scrollTo(0,0),setIsOpen(false)}} to ='/movies'>Movies</Link>
-            <Link onClick={()=>{scrollTo(0,0),setIsOpen(false)}} to ='/'>Theaters</Link>
-            <Link onClick={()=>{scrollTo(0,0),setIsOpen(false)}} to ='/'>Releases</Link>
-            <Link onClick={()=>{scrollTo(0,0),setIsOpen(false)}} to ='/favorite'>Favorites</Link>
+            <Link onClick={()=>{scrollTo(0,0);setIsOpen(false)}} to ='/'>Home</Link>
+            <Link onClick={()=>{scrollTo(0,0);setIsOpen(false)}} to ='/movies'>Movies</Link>
+            <Link onClick={()=>{scrollTo(0,0);setIsOpen(false)}} to ='/'>Theaters</Link>
+            <Link onClick={()=>{scrollTo(0,0);setIsOpen(false)}} to ='/'>Releases</Link>
+            <Link onClick={()=>{scrollTo(0,0);setIsOpen(false)}} to ='/favorite'>Favorites</Link>
         </div>
 
         <div className='flex items-center gap-8'>
@@ -41,7 +43,13 @@ const NavBar = () => {
                 !user ? (
                     <button onClick={openSignIn} className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary0dull transition rounded-full font-medium cursor-pointer'>Login</button>
                 ):(
-                    <UserButton />
+                    <UserButton>
+                        <UserButton.MenuItems>
+                            
+                            <UserButton.Action label="My Bookings" labelIcon={<TicketPlus width={15} />} onClick={() => navigate('/my-bookings')} />
+
+                        </UserButton.MenuItems>
+                    </UserButton>
                 )
             }
         </div>
